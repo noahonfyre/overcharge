a2dfe33921870f6163280974452488097f299cbf
NoahOnFyre <noahonfyre@gmail.com>
2025-05-25 23:57:31 +0200

    feat: add attribute modifier interface

---
+++ src/main/kotlin/com/nyronium/overcharge/content/item/extensions/ItemAttributeModifierExtension.kt
@@ -0,0 +1,18 @@
+package com.nyronium.overcharge.content.item.extensions
+
+import java.util.UUID
+
+// Credits to lender544 on GitHub: https://github.com/lender544/new1.20.1/blob/master/src/main/java/com/github/L_Ender/cataclysm/items/More_Tool_Attribute.java
+interface ItemAttributeModifierExtension {
+    val BASE_ENTITY_INTERACTION_RANGE_ID: UUID
+        get() = UUID.fromString("0CB612AF-CE7C-4FD2-9647-4BFD75B8D8A0")
+    val BASE_BLOCK_INTERACTION_RANGE_ID: UUID
+        get() = UUID.fromString("E7902C57-6C37-41CB-BBC4-F23AB1F287C0")
+
+    val BASE_ARMOR_ID: UUID
+        get() = UUID.fromString("FFB9296F-A5B3-4215-8196-4091D31575C6")
+    val BASE_ARMOR_TOUGHNESS_ID: UUID
+        get() = UUID.fromString("4A87FDB4-7CCC-471D-96C4-03AC8515E62F")
+    val BASE_KNOCKBACK_RESISTANCE_ID: UUID
+        get() = UUID.fromString("0F9ECB41-87F5-4787-8731-0A6C693B77F3")
+}
