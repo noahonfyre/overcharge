c1e222bc0bb07784b6d9851facdf6f52d417a09d
NoahOnFyre <noahonfyre@gmail.com>
2025-05-26 00:39:50 +0200

    fix(datagen): add missing keybind translations

--- src/main/kotlin/com/nyronium/overcharge/infrastructure/datagen/ModLanguageProvider.kt
+++ src/main/kotlin/com/nyronium/overcharge/infrastructure/datagen/ModLanguageProvider.kt
@@ -4,13 +4,16 @@ import com.nyronium.overcharge.Overcharge
 import com.nyronium.overcharge.registry.ModBlocks
 import com.nyronium.overcharge.registry.ModFluidProperties
 import com.nyronium.overcharge.registry.ModItems
+import com.nyronium.overcharge.registry.ModKeyBindings
 import earth.terrarium.botarium.common.registry.fluid.BotariumLiquidBlock
 import net.minecraft.data.PackOutput
-import net.minecraft.world.level.block.LiquidBlock
 import net.minecraftforge.common.data.LanguageProvider
 
 class ModLanguageProvider(packOutput: PackOutput) : LanguageProvider(packOutput, Overcharge.ID, "en_us") {
     override fun addTranslations() {
+        add(ModKeyBindings.KEY_CATEGORY_OVERCHARGE, "Overcharge")
+        add(ModKeyBindings.KEY_TOGGLE_FLIGHT, "Toggle Flight")
+
         for (item in ModItems.ITEMS.entries) {
             val id = item.id ?: continue
             val itemName = formatTranslation(id.path)
