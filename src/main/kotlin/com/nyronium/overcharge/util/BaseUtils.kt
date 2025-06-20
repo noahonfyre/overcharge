22703738fad976962b8790348f7a2d5adbc89b5f
NoahOnFyre <noahonfyre@gmail.com>
2025-06-20 16:23:39 +0200

    feat: add helper to combine multimaps.

--- src/main/kotlin/com/nyronium/overcharge/util/BaseUtils.kt
+++ src/main/kotlin/com/nyronium/overcharge/util/BaseUtils.kt
@@ -1,7 +1,17 @@
 package com.nyronium.overcharge.util
 
+import com.google.common.collect.HashMultimap
+import com.google.common.collect.Multimap
+
 object BaseUtils {
     fun chance(percentage: Int): Boolean {
         return (0..100).random() <= percentage
+    }
+
+    fun <K, V> combinedMultiMaps(vararg maps: Multimap<K, V>): Multimap<K, V> {
+        return maps.fold(HashMultimap.create<K, V>()) { acc, map ->
+            acc.putAll(map)
+            acc
+        }
     }
 }
